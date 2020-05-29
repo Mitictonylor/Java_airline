ee819886a5ba65454d5b2dd7c90d7e9b18f2214a
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-29 13:02:32 +0100

    add Plane class test pass

---
+++ src/test/java/PlaneTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaneTest {
+    private Plane plane;
+
+    @Before
+    public void setUp(){
+        plane = new Plane(PlaneType.BOEING);
+    }
+    @Test
+    public void canGetPlaneType(){
+        assertEquals(PlaneType.BOEING, plane.getPlaneType() );
+    }
+}
