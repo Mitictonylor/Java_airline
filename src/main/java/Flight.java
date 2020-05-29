3a4ff0e019a391fad740017d078f95dbfcb13def
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-29 13:51:41 +0100

    add Flight class and create the flight Test

---
+++ src/main/java/Flight.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Flight {
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destinationAirport;
+    private String departureAirport;
+    private String departureTime;
+
+    public Flight(Plane plane, String flightNumber, String destinationAirport, String departureAirport, String departureTime) {
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destinationAirport = destinationAirport;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+        this.passengers = new ArrayList<Passenger>();
+    }
+
+}
---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+
+public class FlightTest {
+
+    Flight flight;
+    Plane plane;
+    Passenger passenger1;
+    Passenger passenger2;
+    Passenger passenger3;
+    Passenger passenger4;
+
+    @Before
+    public void setUp(){
+        passenger1 = new Passenger("Giovanni", 3);
+        passenger2 = new Passenger("PierGiorgio", 2);
+        passenger3 = new Passenger("Eustachio", 1);
+        passenger4 = new Passenger("Ciccillo", 5);
+        plane = new Plane(PlaneType.BOEING);
+        flight = new Flight(plane,"a1010","BRI","GLA","18:00");
+    }
+
+
+
+}
