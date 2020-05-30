8ea6c4eb6a43c8bfd6def299714d496ae3f1de96
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-30 14:00:25 +0100

    create the binary search algorithm to give back the passenger with the given seat number

--- src/main/java/FlightManager.java
+++ src/main/java/FlightManager.java
@@ -1,16 +1,19 @@
+import java.util.ArrayList;
+
 public class FlightManager {
 
     private Flight flight;
 
-    public FlightManager(Flight flight){
+    public FlightManager(Flight flight) {
         this.flight = flight;
     }
 
-    public double availableBaggageWeightForPlane(){
-        double allowedWeight = this.flight.getPlane().getPlaneType().getTotalWeight()/2;
+    public double availableBaggageWeightForPlane() {
+        double allowedWeight = this.flight.getPlane().getPlaneType().getTotalWeight() / 2;
         return allowedWeight;
     }
-    public double oneBaggageWeight(){
+
+    public double oneBaggageWeight() {
 
         double baggageWeightParPassenger = this.availableBaggageWeightForPlane() / this.flight.getPlane().getPlaneType().getCapacity();
         return baggageWeightParPassenger; //each baggage is 7.5 kg for the concorde
@@ -18,8 +21,8 @@ public class FlightManager {
 
     public double totalBaggageWeighParFlight() {
         double total = 0.00;
-        for( Passenger passenger : this.flight.getPassengers()){
-            total += (passenger.getBags()*this.oneBaggageWeight());
+        for (Passenger passenger : this.flight.getPassengers()) {
+            total += (passenger.getBags() * this.oneBaggageWeight());
         }
         return total;
     }
@@ -28,17 +31,44 @@ public class FlightManager {
         double difference = this.availableBaggageWeightForPlane() - this.totalBaggageWeighParFlight();
         return difference;
     }
-    public void sortPassengerListBySeatNumber(){
-        int i,j = 0;
+
+    public void sortPassengerListBySeatNumber(ArrayList<Passenger> passengers) {
+        int i, j = 0;
         Passenger temp;
-        for (i=0; i<this.flight.countPassenger() - 1; i++){
-            for (j=0; j <this.flight.countPassenger() -1 - i; j++){
-                if(this.flight.getPassengerAtIndex(j).getSeatNumber() > flight.getPassengerAtIndex(j+1).getSeatNumber()){
-                    temp = this.flight.getPassengerAtIndex(j);
-                    this.flight.getPassengers().set(j, this.flight.getPassengerAtIndex(j+1));
-                    this.flight.getPassengers().set(j+1, temp);
+        for (i = 0; i < passengers.size() - 1; i++) {
+            for (j = 0; j < passengers.size() - 1 - i; j++) {
+                if (passengers.get(j).getSeatNumber() > passengers.get(j + 1).getSeatNumber()) {
+                    temp = passengers.get(j);
+                    passengers.set(j, this.flight.getPassengerAtIndex(j + 1));
+                    passengers.set(j + 1, temp);
                 }
             }
         }
     }
-}
+
+    //need to sort the list before using this
+    public Passenger findAPassengerWithBinarySearch(ArrayList<Passenger> passengers, int searchSeatNumber) {
+        int middleIndex = 0;
+        if (passengers.size() > 0) {
+            middleIndex = (int) Math.ceil(passengers.size() / 2);
+        }
+            Passenger middlePerson = passengers.get(middleIndex);
+            if (middlePerson.getSeatNumber() == searchSeatNumber) {
+                return middlePerson;
+            }
+            ArrayList<Passenger> newSearchArea;
+            if (searchSeatNumber < middlePerson.getSeatNumber()) {
+                newSearchArea = new ArrayList<Passenger>(passengers.subList(0, middleIndex));
+            } else {
+                newSearchArea = new ArrayList<Passenger>(passengers.subList((middleIndex + 1), passengers.size()));
+            }
+            return this.findAPassengerWithBinarySearch(newSearchArea, searchSeatNumber);
+        }
+
+        public Passenger sortAndFindPassengerBySeatNumber(ArrayList<Passenger> passengers, int seatNumber){
+         this.sortPassengerListBySeatNumber(passengers);
+         return this.findAPassengerWithBinarySearch(passengers, seatNumber);
+        }
+    }
+
+
